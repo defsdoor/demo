5b7d3dac8f665526205b2d779475b780c51371a6
Andrew Porter <andrew.porter@supplypoint.com>
2021-06-21 08:53:52 +0100

    run chroium or chromium-browser - whichever exists

--- bin/kiosk.sh
+++ bin/kiosk.sh
@@ -7,4 +7,13 @@ start-pulseaudio-x11
 
 x11vnc -display :0 -rfbauth /etc/.vncpasswd -bg -forever
 
-chromium --kiosk --incognito --disable-pinch --no-touch-pinch --overscroll-history-navigation=2 --no-first-run --no-default-browser-check http://localhost:3000
+if type chromium >/dev/null 2>&1
+then
+	BROWSER=chromium
+else
+	BROWSER=chromium-browser
+fi
+
+
+$BROWSER --kiosk --incognito --disable-pinch --no-touch-pinch --overscroll-history-navigation=2 --no-first-run --no-default-browser-check http://localhost
+
